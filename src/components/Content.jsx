d87ded5e6ea1956677f915908257cce18643f82b
Rahul Reghunath <rahulreghunath11@gmail.com>
2022-07-17 20:28:06 +0530

    Label changes

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -4,7 +4,7 @@ class Content extends Component {
     render() {
         return (
             <div className='container'>
-                <h1 style={{userSelect:'none'}} className='name' aria-label='My name is Rahul R'>Rahul Reghunath</h1>
+                <h1 style={{userSelect:'none'}} className='name' aria-label='My name is Rahul R'>Rahul R</h1>
                 <h2 style={{userSelect:'none'}} className='title' aria-label='I am a software engineer'>Software Developer</h2>
             </div>
         )
